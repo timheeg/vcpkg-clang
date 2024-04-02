02f84fccf12759cf8d2dc3ea5bc48a5839fd208d
Timothy Heeg <timheeg@users.noreply.github.com>
2024-04-02 15:15:11 -0400

    🔧 Add clang triplet and toolchain

---
+++ tools/vcpkg/toolchain/clang-16.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER "/usr/bin/clang-16")
+set(CMAKE_CXX_COMPILER "/usr/bin/clang++-16")
+
+message("clang-16 toolchain CMAKE_C_COMPILER = ${CMAKE_C_COMPILER}")
+message("clang-16 toolchain CMAKE_CXX_COMPILER = ${CMAKE_CXX_COMPILER}")
